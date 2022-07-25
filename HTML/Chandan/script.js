7ceb2972c4525510ad151334bdd2ad9ec57e2897
Chandan Pandey <chandan.dv682@gmail.com>
2022-07-25 22:13:05 +0530

    Made some changes.
    Added cart box which will display no of items. 
    Pizza image will now change according to user selection and the selected item will be displayed.
    Also, Changed some colors.
    Added some more social media icons.

--- HTML/Chandan/script.js
+++ HTML/Chandan/script.js
@@ -1,3 +1,49 @@
-function getFood(){
-    document.getElementById('selected').innerHTML=("Item Name : "+formMenu.menu[formMenu.menu.selectedIndex].text)
+let selection = document.querySelector('select');
+let result = document.querySelector('label');
+let msg = document.querySelector('h3');
+let imgD = document.getElementById('pizImg');
+let withI = document.getElementById('with');
+let cartCount = document.getElementById('item_count');
+var count = 1;
+
+selection.addEventListener('change',()=>{
+    msg.innerText = "Great Choice 👍\n";
+    result.innerText = selection.options[selection.selectedIndex].value;
+    if(selection.selectedIndex==0){
+        imgD.src = "images/pizza.png";
+    }
+    else if(selection.selectedIndex==1){
+        imgD.src = "images/pizza1.png";
+    }
+    else if(selection.selectedIndex==2){
+        imgD.src = "images/pizza2.png";
+    }
+    else if(selection.selectedIndex==3){
+        imgD.src = "images/pizza3.png";
+    }
+    else if(selection.selectedIndex==4){
+        imgD.src = "images/pizza4.png";
+    }
+});
+
+function addtoCart()
+{
+    count += 1;
+    cartCount.innerText = count;
+}
+
+function rmCart()
+{
+    count -= 1;
+    if(count>0){
+        cartCount.innerText = count;
+    }
+    else{
+        count = 0;
+        cartCount.innerText = count;
+    }
+}
+function disp()
+{
+    window.alert("Development Ongoing 😊 !");
 }
